9943edb1b63a3d336d5c2f180d2ba82327610f87
fm <wangjiuyin@jiemian.com>
2017-05-19 10:30:02 +0800

    Initial Commit

---
+++ FMSegmentControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMSegmentControl
+//
+//  Created by fm on 2017/5/19.
+//  Copyright © 2017年 wangjiuyin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FMSegmentControl/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FMSegmentControl
+//
+//  Created by fm on 2017/5/19.
+//  Copyright © 2017年 wangjiuyin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
