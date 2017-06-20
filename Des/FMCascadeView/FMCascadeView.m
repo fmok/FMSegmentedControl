35c46a75d7aa74d2cef471c952aba5ecc871283c
fm <wangjiuyin@jiemian.com>
2017-06-20 10:40:02 +0800

    添加图片

---
+++ Des/FMCascadeView/FMCascadeView.m
@@ -0,0 +1,37 @@
+//
+//  FMCascadeView.m
+//  FMCascadeView
+//
+//  Created by fm on 2017/6/20.
+//  Copyright © 2017年 wangjiuyin. All rights reserved.
+//
+
+#import "FMCascadeView.h"
+
+@interface FMCascadeView()
+
+@end
+
+@implementation FMCascadeView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+#pragma mark - getter & setter
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FMSegmentedControl
+
+![](https://github.com/fmok/FMSegmentedControl/raw/master/ScreenShots/1.png)
+![](https://github.com/fmok/FMSegmentedControl/raw/master/ScreenShots/2.png)
+![](https://github.com/fmok/FMSegmentedControl/raw/master/ScreenShots/3.png)
+![](https://github.com/fmok/FMSegmentedControl/raw/master/ScreenShots/4.png)
